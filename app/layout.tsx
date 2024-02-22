8d05f734b32f1fdcf1dcc324fdb6c62771ab36c0
Aquiles Popovich <popovichaquiles@gmail.com>
2024-02-22 19:32:31 -0300

    configuracion de redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/navbar/NavBar";
+import { Providers } from "@/redux/provider";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
       <NavBar />
         {children}
         <p>Footer made in Layout</p>
+        </Providers>
         </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from 'react';
 import { montserrat, inter } from "./ui/fonts";
 import styles from './landing.module.css';
 import banner from '../public/logos/bannerGif.gif'
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { decrement, increment } from "@/redux/features/counterSlice";
 
 export default function Home() {
+  const count = useAppSelector(state => state.counterReducer.counter)
+  const dispatch = useAppDispatch()
+
   return (
     <div className = {styles.holeContainer}>
       <Image src={banner} alt = 'Banner' width = {1150} height = {400}/>
@@ -14,7 +20,18 @@ export default function Home() {
           Color Prueba
         </h1>
       </div>
+      <h2>{count}</h2>
+    <button onClick={()=>{
+      dispatch(increment())
+    }}>
+      increment
 
+    </button>
+    <button onClick={()=>{
+      dispatch(decrement())
+    }}>
+      decrement
+    </button>
     </div>
   );
 }
