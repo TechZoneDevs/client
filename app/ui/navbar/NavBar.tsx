7d6653d864b966f9c93537f15c5327b2584f13ce
Aquiles Popovich <popovichaquiles@gmail.com>
2024-02-25 15:12:02 -0300

    nav Corregido

--- app/ui/navbar/NavBar.tsx
+++ app/ui/navbar/NavBar.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 import { montserrat } from './../fonts';
 import Image from 'next/image';
 import logo from '../../../public/logos/Logo TechZone.png'
-import GradeIcon from '@mui/icons-material/Grade';
+import menuLogo from '../../../public/logos/menuNavBar.png'
+import carritoLogo from '../../../public/logos/carritoLogo.png'
+
+
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SearchIcon from '@mui/icons-material/Search';
 
@@ -14,19 +17,28 @@ export default function NavBar() {
         <div className={styles.navBarContainer}>
             <div className={styles.divLogo}>
                 <Link href={'/'}>
-                    <Image src= {logo} alt='logo' height={100} width={100} />
+                    <Image src= {menuLogo} alt='menu Logo' height={100} width={100} />
                 </Link>
                 <Link href={'/'}>
-                <h1 className={`${montserrat.className} hover:text-shadow-white`} style={{ fontSize: '18px' }}>TechZone</h1>
+                <h1 className={`${montserrat.className} hover:text-shadow-white`} style={{ fontSize: '18px' }}>
+    Tech <span className="relative">
+      <span className="text-black bg-purple-500 p-1 rounded-tl-lg rounded-br-lg">Zone</span>
+    </span>
+  </h1>
                 </Link>
             </div>
             <div>
                 <input type='text' placeholder='Busca un producto' className = "w-[380px] h-[40px] rounded-[30px] p-5 text-black"/>
+                <h2><SearchIcon /></h2>
             </div>
             <div className ="flex items-center justify-center gap-8">            
-            <Link href={'/favoritos'}><h1 className = {`${montserrat.className}`} style={{ fontSize: '18px' }}><GradeIcon />Favoritos</h1></Link>
-            <Link href={'/search'}><h1 className = {`${montserrat.className}`} style={{ fontSize: '18px' }}><SearchIcon />Buscar</h1></Link>
-            <Link href={'/profile'}><h1 className = {`${montserrat.className}`} style={{ fontSize: '18px' }}><AccountCircleIcon />Perfil</h1></Link>
+          
+            <Link href={'/search'}><h2 className = {`${montserrat.className}`} style={{ fontSize: '18px' }}>Productos</h2></Link>
+            <Link href={'/profile'}><h2 className = {`${montserrat.className}`} style={{ fontSize: '18px' }}><AccountCircleIcon />Perfil</h2></Link>
+            <Link href={'/carrito'}><Image src= {carritoLogo} alt='menu Logo' height={80} width={80} /></Link>
+
+           
+            
             </div>
         </div>
     )
