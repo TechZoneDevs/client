d5793fc668c2eb6606330f36337bf69434172624
Leonardo Risco <leo99153@gmail.com>
2024-02-29 00:17:05 -0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <div className={styles.divOne}>
         <div className = {styles.cuadroInfo}>
         <p className={`${montserrat.className}`} style = {{fontSize: '35px',  height: 'auto', fontWeight: '700', textAlign: 'center' }}>Bienvenido a Tech<span className = {styles.span1}>Zone</span></p>
-        <p className={`${montserrat.className}`} style = {{fontSize: '25px',  height: 'auto', fontWeight: '400', textAlign: 'center' }}>Todos tus componentes <span className = {styles.span1} style = {{fontWeight: '600'}}>Tech</span> en un solo lugar</p>
+        <p className={`${montserrat.className}`} style = {{fontSize: '28px',  height: 'auto', fontWeight: '400', textAlign: 'center' }}>Todos tus componentes <span className = {styles.span1} style = {{fontWeight: '600'}}>Tech</span> en un solo lugar</p>
         <div className = {styles.buttons}>
           <button className={styles.boton1}><a href="#category">Categorias</a></button>
           <button className={styles.boton2}><a href="./subhtml/login.html">Nosotros</a></button>
