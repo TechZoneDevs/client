08aa90bda1c96145608cab93671c50d80e4315cb
Leonardo Risco <leo99153@gmail.com>
2024-03-11 01:40:21 -0500

    trabajando en el aboutWe

--- app/ui/about/aboutWe.tsx
+++ app/ui/about/aboutWe.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import logo from '@/public/logos/Logo TechZone.png'
+import logo from '@/public/logos/logoSinFondo.png';
 import Image from 'next/image';
 import styles from "./about.module.css";
 
 const AboutWe = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.containerTecnologias}>
-        <h3><span className={styles.spanHenry2}>Tecnologias utilizadas</span> en este proyecto</h3>
-        <div>
-          aca agregar imagenes de todas las tecnologias usadas
-        </div>
-      </div>
       <div className={styles.containerTech}>
         <div>
           <Image src={logo} alt='logo TechZone' />
         </div>
         <div>
-          <div>
-            <h2>Que es <span className={styles.spanHenry2}>TechZone</span>?</h2>
-          </div>
           <p>Es un  <span className={styles.spanHenry2}>Ecommerce completo</span> realizado con un equipo de <span className={styles.spanHenry}>desarrolladores</span> egresados en <span className={styles.spanHenry}>soyHenry</span> con el fin de incorporar experiencia y tecnologias nuevas para incrementar nuestras <span className={styles.spanHenry}>oportunidades laborales</span> como desarrolladores a tiempo completo. Nos especializamos en <span className={styles.spanHenry2}>diversas tecnologías </span> y estamos comprometidos con la <span className={styles.spanHenry}>excelencia</span> en nuestro trabajo.</p>
         </div>
       </div>
